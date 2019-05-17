09af380dac6c33a81dd8685c3af30ef7447e7f8a
StevenMcGown <46934571+StevenMcGown@users.noreply.github.com>
2019-05-17 15:48:04 -0400

    Update ReverseInt.cc

--- ReverseInt.cc
+++ ReverseInt.cc
@@ -2,14 +2,14 @@ class ReverseInt {
 public:
     int reverse(int x) {
         long int ans=0; // long int range is 32 bit, long long is 64 bit
-        if(ans>INT_MAX)
-           return 0;
-        else if(ans<INT_MIN)
-           return 0;
         while(x) // While x != 0 
         {
-            ans=(ans*10) +(x%10); 
-            x/=10;
+          ans=(ans*10) +(x%10); 
+          x/=10;
+          if(ans>INT_MAX)
+            return 0;
+          if(ans<INT_MIN)
+            return 0;
         }
         return ans;
     }
