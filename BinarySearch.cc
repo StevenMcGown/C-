072b626af25bc73b141ee631b1742d561ccc4c90
StevenMcGown <46934571+StevenMcGown@users.noreply.github.com>
2019-05-20 14:29:33 -0400

    Update BinarySearch.cc
    Updated for faster execution time, less memory used

--- BinarySearch.cc
+++ BinarySearch.cc
@@ -7,19 +7,15 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class binarySearch {
+class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        if (root == NULL)
-            return NULL;
-        if (root->val == val) {
-            return root;
-        } else {
-            if (val < root->val)
-                return searchBST(root->left, val);
-            else {
-                return searchBST(root->right, val);
-            }
-        }
+    TreeNode* searchBST(TreeNode* root, int val) 
+    {
+         if(root==NULL || root->val==val)
+             return root;
+         if(root->val>val)
+             return searchBST(root->left,val);
+         else
+             return searchBST(root->right,val);  
     }
 };
