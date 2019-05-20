f6a29336d6bdb42d1fb76d588b33ce2e1bb2085e
StevenMcGown <46934571+StevenMcGown@users.noreply.github.com>
2019-05-20 09:49:28 -0400

    Create longestCommonPrefix.cc
    Returns a common prefix from all strings in a vector

---
+++ longestCommonPrefix.cc
@@ -0,0 +1,19 @@
+class longestCommonPrefix {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0)
+            return "";
+        for (int i = 0; i < strs[0].length(); ++i) {
+           char c = strs[0].at(i); // Iterate through each character in first string
+           for (int j = 1; j < strs.size(); ++j) {
+               /* If the last index of the first string is reached or 
+                * the character at common index is not identical, return
+                * the substring from 0 to the current index
+                */
+               if (i == strs[j].length() || strs[j].at(i) != c) 
+                   return strs[0].substr(0, i);
+           }
+        }
+        return strs[0];
+    }
+};
